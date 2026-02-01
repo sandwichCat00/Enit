444f7abf3d17b2049a0be9c7c412d403176c0fe3
Samyak1245 <samyakpusate111@gmail.com>
2026-02-01 10:39:27 +0530

    chore: intit flask2

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = "campus_secret_key"
+
+# --- COMMON ROUTES ---
 
 @app.route('/')
-def home():
-    return render_template('index.html', title="Welcome Page")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login/student')
+def student_login():
+    
+    return render_template('auth/student_login.html')
+
+@app.route('/login/company')
+def company_login():
+    return render_template('auth/company_login.html')
+
+
+
+@app.route('/company/dashboard')
+def company_dashboard():
+    return render_template('company/dashboard.html')
+
+@app.route('/company/post-job')
+def post_job():
+    return render_template('company/post_job.html')
+
+@app.route('/company/applications')
+def view_applications():
+    return render_template('company/application.html')
+
+
+@app.route('/student/dashboard')
+def student_dashboard():
+   
+    return render_template('student/dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
